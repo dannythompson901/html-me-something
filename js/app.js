4131f0eddacfeb5988a89c0988e8f9ecc9cc6038
Danny Thompson <dannythompson901@gmail.com>
2019-03-29 12:37:14 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var backgrounds = [
     { src: 'img/01.jpg',  },
     { src: 'img/04.jpg',  },
     { src: 'img/03.jpg',  },
-    { src: 'img/04.JPG', delay: 1000, video: [
+    { src: 'img/01.jpg', delay: 1000, video: [
         
     ] }
 ];
@@ -16,8 +16,8 @@ var displayBackdrops = false;
 $body.vegas({
     preload: true,
     overlay: '/js/01.png',
-    transitionDuration: 1000,
-    delay: 3000,
+    transitionDuration: 1500,
+    delay: 4000,
     slides: backgrounds,
     walk: function (nb, settings) {
         if (settings.video) {
